5e6058aad9356efec031fa8935d7e64b2dfe6d25
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-06-26 15:07:19 -0400

    Remove the unnecessary link

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {sequelize = new Sequelize(
+} else { sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
